49e3a2b8eb40ca5ab4bb5145efba6a4dc5cf1422
Yuki <yukitang@mail.bnu.edu.cn>
2020-11-14 22:20:29 +0800

    11.14

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -1,3 +1,11 @@
+'''
+Author: your name
+Date: 2020-11-09 20:45:01
+LastEditTime: 2020-11-14 22:19:56
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /leetcode-python/leetcode-python/3.longest-substring-without-repeating-characters.py
+'''
 #
 # @lc app=leetcode id=3 lang=python3
 #
@@ -7,6 +15,19 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        if len(s) == 0:return ans
+        max_dict = dict()
+        i = 0
+        for j in range(len(s)):
+            if s[j] in max_dict:
+                i = max(i, max_dict.get(s[j]))
+            ans = max(ans, j-i+1)
+            # max_dict represents the place next i should be(curent j+1)
+            max_dict[s[j]] = j+1
+        return ans
+                
+        
         
 # @lc code=end
 
