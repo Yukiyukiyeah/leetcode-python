17d86366003a4c0261c5223c3a78ffcc28990cf2
Yuki <yukitang@mail.bnu.edu.cn>
2020-12-01 16:39:49 +0800

    11.30

---
+++ 213.house-robber-II.py
@@ -0,0 +1,24 @@
+'''
+Author: your name
+Date: 2020-12-01 16:39:40
+LastEditTime: 2020-12-01 16:39:40
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /leetcode-python/213.house-robber-II.py
+'''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums: return 0
+        if len(nums) == 1: return nums[0]
+        
+        def rob1(nums, start, end):
+            pre = 0
+            cur = 0
+            for i in range(start, end):
+                tmp = cur
+                cur = max(cur, pre + nums[i])
+                pre = tmp
+            return cur
+        
+        return max(rob1(nums, 0, len(nums) - 1), rob1(nums, 1, len(nums)))
+        
